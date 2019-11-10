915ac371eaeca19d1743def8599c7db27cfc0a2a
Sho Minagawa <msh5.global@gmail.com>
2019-11-10 11:17:06 +0900

    Fix usage

--- interface/cmd/exec.go
+++ interface/cmd/exec.go
@@ -27,13 +27,8 @@ import (
 // execCmd represents the exec command
 var execCmd = &cobra.Command{
 	Use:   "exec",
-	Short: "Execute on-cloud snippet directly",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "TODO",
+	Long:  `TODO`,
 	Run: func(cmd *cobra.Command, args []string) {
 		config := loadCommandConfig()
 
--- interface/cmd/root.go
+++ interface/cmd/root.go
@@ -30,13 +30,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "boy",
-	Short: "On-Cloud Snippet Runner",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "On-cloud snippet runner",
+	Long:  `TODO`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
