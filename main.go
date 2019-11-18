b185aab056b40a30277ccc1ece7c8802c034661d
Sho Minagawa <msh5.global@gmail.com>
2019-11-18 23:25:35 +0900

    Support --version flag

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
-/*
-Copyright © 2019 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package main
 
-import "github.com/msh5/boy/interface/cmd"
+import (
+	"fmt"
+
+	"github.com/msh5/boy/interface/cmd"
+)
+
+// "version", "commit" and "date" variables are overridden by goreleaser.
+
+var (
+	version = "dev"
+	commit  = "none"    // nolint:gochecknoglobals
+	date    = "unknown" // nolint:gochecknoglobals
+)
+
+func formatVersionString() string {
+	return fmt.Sprintf("%v, commit %v, built at %v", version, commit, date)
+}
 
 func main() {
+	cmd.SetVersion(formatVersionString())
+
 	cmd.Execute()
 }
